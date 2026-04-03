d43183a87b635cd34fd395a3afd255a9a9a73aba
kayh09 <h.kayleigh@wustl.edu>
2026-04-03 14:19:11 -0400

    click x on the squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-function Square({ value }) {
-    function handleClick() {
-    console.log('clicked!');
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
   }
-  return <button className="square">{value}</button>;
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
