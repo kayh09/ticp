ebdc9d3a2cb6ad500789e043c9417805ae6a5aa6
kayh09 <h.kayleigh@wustl.edu>
2026-04-03 15:14:45 -0400

    finished the step completing the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -13,15 +13,14 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-      if (squares[i]) {
-    return;
-  }
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-     if (xIsNext) {
-      nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = "O";
+      nextSquares[i] = 'O';
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
