3b9b3e007fe94ede1c6981703c872983f4fccba7
kayh09 <h.kayleigh@wustl.edu>
2026-04-03 15:30:26 -0400

    x or o win

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -43,6 +43,13 @@ export default function Board() {
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
+      const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   }
 
   return (
